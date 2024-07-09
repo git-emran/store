fb1045811d29b96e6bb0e92102123ed7e01707a9
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-07-09 10:01:51 +0600

    zod library error handling added on the create product page

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,6 +3,9 @@
 import db from "@/utils/db";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { productSchema } from "./schemas";
+import { raw } from "@prisma/client/runtime/library";
+import { error } from "console";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -60,23 +63,13 @@ export const createProductAction = async (
   const user = await getAuthUser();
 
   try {
-    const name = formData.get("name") as string;
-    const company = formData.get("company") as string;
-    const price = Number(formData.get("price") as string);
-    const image = formData.get("image") as File;
-    const description = formData.get("description") as string;
-    const featured = Boolean(formData.get("featured") as string);
-    await db.product.create({
-      data: {
-        name,
-        company,
-        price,
-        image: "/images/product-1.jpg",
-        description,
-        featured,
-        clerkId: user.id,
-      },
-    });
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = productSchema.safeParse(rawData);
+
+    if (!validatedFields.success) {
+      const errors = validatedFields.error.errors.map((error) => error.message);
+      throw new Error(errors.join(","));
+    }
 
     return { message: "product created" };
   } catch (error) {
